2803fc0db9ef2dcc0e03474860071dd4b2f659c9
mabsoares <mabsoares@hcpa.edu.br>
2024-01-30 08:24:34 -0300

    ajuste projeto

---
+++ CLIENTE_SERVICE/clienteServices/src/main/java/com/mbs/clienteServices/ClienteServicesApplication.java
@@ -0,0 +1,13 @@
+package com.mbs.clienteServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteServicesApplication.class, args);
+	}
+
+}
