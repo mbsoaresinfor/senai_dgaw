47a28c6d531497179af97e6260facce49c534eb6
marcelo soares <mbsoares.infor@gmail.com>
2024-03-25 22:15:29 -0300

    aula 6

---
+++ VENDAS_SERVICES/vendasServices/src/main/java/com/mbs/vendasServices/controller/VendasControllerAPI.java
@@ -0,0 +1,47 @@
+package com.mbs.vendasServices.controller;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mbs.vendasServices.entidades.Venda;
+import com.mbs.vendasServices.servico.VendasServico;
+
+@RestController
+public class VendasControllerAPI {
+
+	@Autowired
+	private VendasServico vendasService;
+	
+	
+	@RequestMapping(value = "/v1/vendas", method = RequestMethod.POST)	
+	public ResponseEntity<Void> salvarVenda(@RequestBody Venda venda) {		
+		System.out.println("processando salvarVenda " + venda);
+		vendasService.salvarVendas(venda);
+		return ResponseEntity.ok().build();
+	}
+	
+	@RequestMapping(value = "/v1/vendas", method = RequestMethod.GET)
+	public ResponseEntity<List<Venda>> listarVendas() {
+		System.out.println("processando listarVendas " );		
+		return ResponseEntity.ok(vendasService.listarVendas());
+	}
+	
+	@RequestMapping(value = "/v1/total_vendas", method = RequestMethod.GET)
+	public ResponseEntity<Double>  totalVendas() {
+		System.out.println("processando totalVendas " );
+		return ResponseEntity.ok(vendasService.totalVendas());
+	}
+	
+	
+}
