0be440ffa72922f91172a653bd32113c28340fd6
Marcelo Barbosa Soares <treinamento@rodosoft.com.br>
2024-06-16 09:14:27 -0300

    ajustes

--- NOTIFICACAO_SERVICE/notificacaoServices/src/main/java/com/mbs/notificacaoServices/NotificacaoServicesApplication.java
+++ NOTIFICACAO_SERVICE/notificacaoServices/src/main/java/com/mbs/notificacaoServices/NotificacaoServicesApplication.java
@@ -1,9 +1,12 @@
 package com.mbs.notificacaoServices;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@EnableRabbit
 public class NotificacaoServicesApplication {
 
 	public static void main(String[] args) {
