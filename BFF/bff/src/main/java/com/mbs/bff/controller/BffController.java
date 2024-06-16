2cb93d5b5f62b6ed786466c599fa3d07c8225936
Marcelo Barbosa Soares <treinamento@rodosoft.com.br>
2024-06-16 09:59:54 -0300

    mensgageria

--- BFF/bff/src/main/java/com/mbs/bff/controller/BffController.java
+++ BFF/bff/src/main/java/com/mbs/bff/controller/BffController.java
@@ -1,6 +1,8 @@
 package com.mbs.bff.controller;
 
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +22,12 @@ public class BffController {
 	@Autowired
 	private VendasService vendasService;
 	
+	@Autowired
+	private AmqpTemplate rabbitTemplate;
+	
+	@Value("${exchange}")
+	private String exchange;
+	
 	// orquestramento de serviços
 	@RequestMapping(value = "/v1/bff", method = RequestMethod.POST)	
 	public ResponseEntity<Boolean> processarVenda(@RequestBody Venda venda){
@@ -33,5 +41,10 @@ public class BffController {
 		
 		vendasService.salvarVenda(venda);
 		return ResponseEntity.ok(true);
+	}
+	
+	@RequestMapping(value = "/v1/teste", method = RequestMethod.GET)	
+		public void teste(){
+		rabbitTemplate.convertAndSend("", exchange, "teste");
 	}
 }
