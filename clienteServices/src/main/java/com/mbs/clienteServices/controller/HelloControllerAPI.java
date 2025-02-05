1e347e7849d5707a74350a5c20eb7ec5472fe076
mabsoares <mabsoares@hcpa.edu.br>
2025-02-05 15:23:57 -0300

    atualizacao

--- clienteServices/src/main/java/com/mbs/clienteServices/controller/HelloControllerAPI.java
+++ clienteServices/src/main/java/com/mbs/clienteServices/controller/HelloControllerAPI.java
@@ -1,4 +1,4 @@
-package com.mbs.vendasServices.controller;
+package com.mbs.clienteServices.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
