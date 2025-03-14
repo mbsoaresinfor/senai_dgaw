dbbde72017b6a3f7c0a61ebb66b8a147238ec4b0
Faculdade SENAI <fatec.senai@UO692M4092131.acad692.com.br>
2025-03-13 22:13:39 -0300

    aula 3

--- enderecoMVC/src/main/java/com/mbs/enderecomvc/controller/EnderecoControllerMVC.java
+++ enderecoMVC/src/main/java/com/mbs/enderecomvc/controller/EnderecoControllerMVC.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.mbs.enderecomvc.entidades.Endereco;
 
@@ -31,20 +32,79 @@ public class EnderecoControllerMVC {
 			@ModelAttribute 
 			Endereco endereco,
 			Model model) {
-		if(endereco.getRua().length() <=3 ) {
+		if(endereco.getRua().trim().length() <=3 ) {
 			model.addAttribute("erro",
 					"Valor  invalido para campo rua");			
 			return "cadastrar";
 		}
-		else {
-			endereco.setCodigo(++id);
-			listaEndereco.add(endereco); // adiciona na lista.
-			System.out.println("cadastrado endereco: " + endereco.getRua());
-			model.addAttribute("lista_endereco",listaEndereco); // adiciona na request para a view pegar.
-			return "resultado";
+		if(endereco.getCep()
+				.trim().contains("-")) {
+			model.addAttribute("erro",
+					"Cep invalido, não digite o caracter '-'");			
+			return "cadastrar";
 		}
 		
+		endereco.setCodigo(++id);
+		listaEndereco.add(endereco); // adiciona na lista.
+		System.out.println("cadastrado endereco: " + endereco.getRua());
+		model.addAttribute("lista_endereco",listaEndereco); // adiciona na request para a view pegar.
+		return "resultado";
 	}
+	
+	@GetMapping("/deletar")
+	public String deletar(@RequestParam String id,
+			Model model) {
+		System.out.println("Deletar: ID = " + id);
+		listaEndereco.removeIf((obj)-> 
+					obj.getCodigo()
+					.equals(Integer.parseInt(id)));
+		model.addAttribute("lista_endereco",listaEndereco); // adiciona na request para a view pegar.
+		return "resultado";
+	}
+	
+	@GetMapping("/inicio_buscar")
+	public String inicioBuscar( Model model) {
+		model.addAttribute("endereco", 
+				new Endereco());
+		return "buscar";
+	}
+	
+	@GetMapping(path="/buscar")
+	public String buscar(
+			@ModelAttribute 
+			Endereco endereco,
+			Model model) {
+		System.out.println("Buscar");
+		StringBuilder resultado = new StringBuilder();
+		
+		for (Endereco end : listaEndereco) {
+			if(end.getCodigo()
+					.equals(endereco.getCodigo())) {
+				resultado
+				.append("cod:")
+				.append(end.getCodigo())
+				.append("rua: ")
+				.append(end.getRua())
+				.append("-----");				
+			}else if(end.getCep()
+					.equals(endereco.getCep())) {
+				resultado
+				.append("cep:")
+				.append(end.getCep())
+				.append("rua: ")
+				.append(end.getRua())
+				.append("-----");
+			}
+		}
+		if(resultado.isEmpty()) {
+			resultado.append("NÃO FOI POSSIVEL "
+					+ "LOCALIZAR NENHUM ENDERECO");
+		}
+		model.addAttribute("resultado", 
+				resultado.toString());
+		return "buscar";
+	}
+	
 	
 	// CONTINUAR AS IMPLEMENTACOES
 }
