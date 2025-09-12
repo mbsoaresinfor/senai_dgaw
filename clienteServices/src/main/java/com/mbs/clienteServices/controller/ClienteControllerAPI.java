ccbd29151f9db656a1335f19f1cb2b5b4d935ebf
MARCELO <mbsoares.infor@gmail.com>
2025-09-11 21:44:13 -0300

    Update ClienteControllerAPI.java
    aula 7

--- clienteServices/src/main/java/com/mbs/clienteServices/controller/ClienteControllerAPI.java
+++ clienteServices/src/main/java/com/mbs/clienteServices/controller/ClienteControllerAPI.java
@@ -15,6 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.mbs.clienteServices.entidades.Cliente;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RestController
 @CrossOrigin(origins = "http://localhost:9005")
 public class ClienteControllerAPI {
@@ -22,7 +26,10 @@ public class ClienteControllerAPI {
 	private List<Cliente> listaCliente = new ArrayList<Cliente>();
 	private static Integer id = 0;
 	
-	
+	@Operation(summary = "Salva um cliente")
+	@ApiResponses(value = 
+			{@ApiResponse(responseCode = "200",description = "Cadastro com sucesso"),
+			@ApiResponse(responseCode = "400",description = "Erro na validação dos campos")})
 	@RequestMapping(value = "/v1/cliente",method = RequestMethod.POST)
 	public ResponseEntity<String> salvar(@RequestBody Cliente cliente) {
 		System.out.println("executando salvar " + cliente);
@@ -39,6 +46,9 @@ public class ClienteControllerAPI {
 		return ResponseEntity.ok(id.toString());
 	}
 	
+	@Operation(summary = "Retorna uma listagem de todos os clientes cadastrados")
+	@ApiResponses(value = 
+			{@ApiResponse(responseCode = "200",description = "Lista de clientes")})
 	@RequestMapping(value = "/v1/cliente",method = RequestMethod.GET)
 	public ResponseEntity<List<Cliente>> listar() {
 		System.out.println("executando listar " );
@@ -46,6 +56,10 @@ public class ClienteControllerAPI {
 		return ResponseEntity.ok(listaCliente);
 	}
 	
+	@Operation(summary = "Deletar um cliente pelo seu id")
+	@ApiResponses(value = 
+	{@ApiResponse(responseCode = "200",description = "Cliente deletado com sucesso"),
+		@ApiResponse(responseCode = "400",description = "Não foi removido cliente")})
 	@RequestMapping(value = "/v1/cliente/{id}",method = RequestMethod.DELETE)
 	public ResponseEntity<Void> deletar(@PathVariable Integer id) {
 		System.out.println("executando deletar de cliente id " + id );
@@ -59,6 +73,10 @@ public class ClienteControllerAPI {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 	}
 	
+	@Operation(summary = "Atualiza um cliente")
+	@ApiResponses(value = 
+	{@ApiResponse(responseCode = "200",description = "Cliente atualizado com sucesso"),
+		@ApiResponse(responseCode = "400",description = "Erro na atualização do cliente")})
 	@RequestMapping(value = "/v1/cliente",method = RequestMethod.PUT)
 	public ResponseEntity<String> atualizar(@RequestBody Cliente cliente) {
 		for(Cliente c : listaCliente) {
@@ -73,6 +91,32 @@ public class ClienteControllerAPI {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 	}
 	
+	@Operation(summary = "Retorna true caso cliente exista ou false se não existir no sistema")
+	@ApiResponses(value = 
+			{@ApiResponse(responseCode = "200",description = "TRUE=existir ou FALSE=não existir")})
+	@RequestMapping(value = "/v1/cliente/existe-cliente/{id}",method = RequestMethod.GET)
+	public ResponseEntity<Boolean> existeCliente(@PathVariable Integer id) { 
+		for(Cliente c : listaCliente) {
+			if(c.getId().equals(id)) {
+				return ResponseEntity.ok(Boolean.TRUE);				
+			}
+		}
+		return ResponseEntity.ok(Boolean.FALSE);		
+	}
 	
-	// SEGUIR IMPLEMENTACAO
+	@Operation(summary = "Retorna um cliente pelo seu ID.")
+	@ApiResponses(value = 
+	{@ApiResponse(responseCode = "200",description = "Cliente retornado com sucesso"),
+	@ApiResponse(responseCode = "204",description = "Não encontrado cliente")})
+	@RequestMapping(value = "/v1/cliente/buscar-cliente/{id}",method = RequestMethod.GET)
+	public ResponseEntity<Cliente> buscarCliente(@PathVariable Integer id) { 
+		for(Cliente c : listaCliente) {
+			if(c.getId().equals(id)) {
+				return ResponseEntity.ok(c);				
+			}
+		}
+		return ResponseEntity.noContent().build();		
+	}
+
+
 }
