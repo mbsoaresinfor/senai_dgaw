5a7d9d3dd8ca379479a7116e1daf3e1a57d81518
aluno230 <aluno230@UO692M4092131.acad692.com.br>
2025-10-30 21:26:03 -0300

    aula 14

--- notificacaoServices/src/main/java/com/mbs/notificacaoServices/ProdutoListener.java
+++ notificacaoServices/src/main/java/com/mbs/notificacaoServices/ProdutoListener.java
@@ -2,7 +2,10 @@ package com.mbs.notificacaoServices;
 
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
@@ -18,12 +21,24 @@ public class ProdutoListener {
 	
 	private Gson gson = new Gson();
 	
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
 	@RabbitListener(queues = {"${queue}"})
     public void receive(@Payload String fileBody) {
 		System.out.println("Mensagem recebida da fila " + queue + ": " +  fileBody);
 		EventoEmail evento = gson.fromJson(fileBody, EventoEmail.class);
 		
 		System.out.println("email do cliente: " +  evento.getCliente().getEmail());
+		
+		// NÃO ESTÁ ENVIANDO EMAIL, POIS NÃO CONSEGUIMOS ENVIAR.
+		SimpleMailMessage message = new SimpleMailMessage(); 
+        message.setFrom("contasecundariapadrao@gmail.com");
+        message.setTo(evento.getCliente().getEmail()); 
+        message.setSubject(evento.getTituloEmail()); 
+        message.setText(evento.getTextoEmail());
+        //javaMailSender.send(message);
+		
 		
     }
 }
---
+++ notificacaoServices/src/main/java/com/mbs/notificacaoServices/configuracoes/Configuracoes.java
@@ -0,0 +1,31 @@
+package com.mbs.notificacaoServices.configuracoes;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Configuracoes {
+
+	@Bean
+	public JavaMailSender getJavaMailSender() {
+	    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+	    mailSender.setHost("smtp.gmail.com");
+	    mailSender.setPort(587);
+	    
+	    mailSender.setUsername("contasecundariapadrao@gmail.com");
+	    mailSender.setPassword("padraoparatestes");
+	    
+	    Properties props = mailSender.getJavaMailProperties();
+	    props.put("mail.transport.protocol", "smtp");
+	    props.put("mail.smtp.auth", "true");
+	    props.put("mail.smtp.starttls.enable", "true");
+	    props.put("mail.debug", "true");
+	    
+	    return mailSender;
+	}
+	
+}
